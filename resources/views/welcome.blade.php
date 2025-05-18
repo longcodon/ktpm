2884264bbcd521c0732467a8c7fe363386cdc32f
longcodon <khaclong02042005@gmail.com>
2025-05-18 17:46:46 +0700

    18-5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,23 +112,26 @@
   <section class="product-gallery-section">
     <h2 class="section-title">Sản Phẩm</h2>
     <div class="product-gallery">
-@foreach($danhmuc as $key => $item)
+    @foreach($danhmuc as $key => $item)
+    @php
+        $videoId = \Illuminate\Support\Str::after($item->link, 'v=');
+      //   print($videoId); 
+      // print($item->link);
+    @endphp
+
     <div class="product-card" onclick="openModal(
-        '{{ $item->title ?? 'Không có tên' }}', 
-        '{{ $item->image ? asset('uploads/danhmuc/'.$item->image) : asset('/images/default.png') }}', 
-        '{{ $item->description ?? 'Đang cập nhật mô tả' }}', 
-        '{{ isset($item->price) ? number_format($item->price, 0, ',', '.').' ₫' : 'Liên hệ' }}', 
-        '{{ $item->author ?? 'Chưa rõ' }}', 
+        '{{ $item->title ?? 'Không có tên' }}',
+        '{{ $item->image ? asset('uploads/danhmuc/'.$item->image) : asset('/images/default.png') }}',
+        '{{ $item->description ?? 'Đang cập nhật mô tả' }}',
+        '{{ isset($item->price) ? number_format($item->price, 0, ',', '.').' ₫' : 'Liên hệ' }}',
+        '{{ $item->author ?? 'Chưa rõ' }}',
         '{{ $item->transcribed ?? 'KChipShop' }}',
-        '{{ $item->link ??  'https://www.youtube.com/embed/mkA2XJmNTO8' }}'
-         
+        'https://www.youtube.com/embed/{{ $videoId }}'
 
 
-
-        
     )">
         <div class="product-image">
-            <img src="{{ $item->image ? asset('uploads/danhmuc/'.$item->image) : asset('/images/default.png') }}" 
+            <img src="{{ $item->image ? asset('uploads/danhmuc/'.$item->image) : asset('/images/default.png') }}"
                  alt="{{ $item->title ?? '' }}" />
         </div>
         <h4>{{ $item->title ?? 'Sản phẩm mới' }}</h4>
@@ -232,6 +235,6 @@
    
   <script src="{{ asset('frontend/js/index.js') }}"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
+  
 </body>
 </html>
